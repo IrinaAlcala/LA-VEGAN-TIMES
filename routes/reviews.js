14f1ee0689edd0ee931f917d659ed37f1aae569f
IrinaAlcala <irinaalcala@Jasons-MBP.fios-router.home>
2020-04-19 20:21:46 -0700

    corrected

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,6 +7,6 @@ var reviewsCtrl = require('../controllers/reviews');
 
 
 router.post('/restaurants/:id/reviews', reviewsCtrl.create);
-router.put('/reviews/:id', reviewsCtrl.update);
+router.put('/reviews/:id/', reviewsCtrl.update);
 router.delete('/reviews/:id', reviewsCtrl.delete);
 module.exports = router;
