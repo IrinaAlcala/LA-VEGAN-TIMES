55b29fe0e24464826061a097a75e9788b7e314b9
IrinaAlcala <irinaalcala@Jasons-MBP.fios-router.home>
2020-04-22 22:20:00 -0700

    final commit

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -16,7 +16,7 @@ function create(req, res) {
   console.log(req.params.id)
   Restaurant.findById(req.params.id, function(err, restaurant) {
     console.log(restaurant)
-    req.body.createdBy = req.user._id;
+    // req.body.createdBy = req.user._id;
         req.body.userName = req.user.name;
         console.log(req.body.userName);
     restaurant.reviews.push(req.body);
@@ -31,7 +31,7 @@ function create(req, res) {
         Restaurant.findById(req.params.restaurantId, function(err, restaurant) {
           console.log(err)
           const review = restaurant.reviews.id(req.params.reviewId);
-          //  if(!review.createdBy.equals(req.user && req.user.id)) return res.redirect(`/restaurants/${restaurant._id}`);
+          // if(!review.createdBy.equals(req.user && req.user.id)) return res.redirect(`/restaurants/${restaurant._id}`);
         review.remove();
           restaurant.save(function(err) {
             res.redirect(`/restaurants/${restaurant._id}`)
@@ -49,7 +49,7 @@ function create(req, res) {
         Restaurant.findOne(req.params.id, function(err, restaurant) {
           const review = restaurant.reviews.id(req.params.id);
           console.log(review);
-          if(!reviewSubdoc.createdBy.equals(req.user && req.user.id)) return res.redirect(`/restaurants/${restaurant._id}`);
+          // if(!reviewSubdoc.createdBy.equals(req.user && req.user.id)) return res.redirect(`/restaurants/${restaurant._id}`);
         req.body.createdBy = req.user._id;
         reviewSubdoc.content = req.body.content;
           restaurant.save(function(err) {
